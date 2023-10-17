a9e46b36806895386bae4c24bebd038bd91b2ff4
khalid Tuhin <76039658+khalid-Ibne-Hussain@users.noreply.github.com>
2023-10-17 22:08:06 +0600

    Time: 52 ms (57.84%), Space: 42.6 MB (11.31%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; j<n; i++,j++ ){
+        nums1[i]= nums2[j];
+    }
+    nums1.sort((a,b)=>a-b);
+};
